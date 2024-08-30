1f83de39112e09e5f40cb5548f0fe96d0f323ef1
Jerimaine Hunt <100004627+JeriJamz@users.noreply.github.com>
2024-08-30 13:41:52 -0500

    Update Kern.py

--- Kern.py
+++ Kern.py
@@ -5,27 +5,37 @@ import time,sys,pyautogui as pag, curses
 
 #refresh()<-update screen after input, noutrefresh()<-change background(just text),doupdate()<- update display
 
-def Boot(self, termin):#see if I can make this the main window wit a boot up and file director
-    self.termin = termin
-    termin = curses.initscr()
+#tweak'd need a class
 
-    termin.print("High-Level Kernal.OS")#This might not work
-    termin.noecho()
-    termin.keypad(True)
-    termin.clear()#The Boot Uplol wheres The ASM windows?
-    termin.addstr()#I should start getting user input... Let me make a menu
+class KernBootWindow:
 
-def oltra(self,End):
-    self.end = end
-
-    termin.nobreak()
-    termin.keypad(False)
-    termin.clear()
-    termin.echo()
+    def __init__(name,boot):
+        self. name = name
+        self.boot = termin
+        name = The Screen
     
-def Term(self,exit):
-    self.exit = exit
-
-    termin.endwin()
+    def Boots(self):#see if I can make this the main window wit a boot up and file director
+        
+        termin = curses.initscr()
+    
+        termin.print("High-Level Kernal.OS")#This might not work
+        termin.noecho()
+        termin.keypad(True)
+        termin.clear()#The Boot Uplol wheres The ASM windows?
+        termin.addstr()#I should start getting user input... Let me make a menu
+    
+    def oltra(self,End):
+        
+        self.end = end
+    
+        termin.nobreak()
+        termin.keypad(False)
+        termin.clear()
+        termin.echo()
+        
+    def Term(self,exit):
+        self.exit = exit
+    
+        termin.endwin()
 
 boot()
