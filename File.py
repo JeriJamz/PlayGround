48629aa0458031de8178c4c4014f6166f8577f13
Jerimaine Hunt <100004627+JeriJamz@users.noreply.github.com>
2024-08-30 13:52:08 -0500

    Create File.py

---
+++ File.py
@@ -0,0 +1,19 @@
+#Need a Menu for the Files... Will this work?
+from Printr import *
+import pprint
+#can I make a server wit python or do I need Django... Ima use Go instead
+
+pp = pprint
+o_p_p = pprint.PrettyPrint(compact = True)
+
+class FileList:
+
+    def __init__(self):
+
+    def menu(self,navBar):#I need a list? User input.. so dictorary inside of an array for rn
+
+        self.navBar = navbar
+
+        navBar = {"(1)":"Folders","(2)":"Settings"}#Gotta remember to add a .addpend method to the Kern not for this Navbar but probably the next
+        pp.qprint(navBar)
+
