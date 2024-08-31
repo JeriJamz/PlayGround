63bdf5bfed507b197b34c53eb17c265c0678f5eb
Jerimaine Hunt <100004627+JeriJamz@users.noreply.github.com>
2024-08-31 11:45:51 -0500

    Create Notepad.py

---
+++ Notepad.py
@@ -0,0 +1,21 @@
+#This is the header file that handles all notepad function. It handles the Files for the FilesDirectory 
+#Working on the user input
+from printr import *
+
+
+class Notepad:
+
+    def __init__(self, pad):
+
+        self.pad = Open
+
+
+    def CreateFile(self):
+
+        response = input()
+        termin.getch(response)
+
+        if response == "Create File".lower():
+            
+        
+        Open = open("","Open", Encoding = "utf-8")#Filename, type, encoding = utf-8
