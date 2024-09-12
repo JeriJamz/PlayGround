9bb784e33ecee7075eadd3ac932249f8e5358248
Jerimaine Hunt <100004627+JeriJamz@users.noreply.github.com>
2024-09-12 17:10:58 -0500

    Update file.py

--- file.py
+++ file.py
@@ -8,8 +8,23 @@ o_p_p = pprint.PrettyPrint(compact = True)
 
 class FileList:
 
-    def __init__(self):
+    def __init__(self,fil):
+        
+        self.fil = file
 
+    def pend(self, files):
+
+        self.files.append(file)
+
+    def Sto(self, fill):
+        
+        fill = []
+
+        if FileList.pend() = +1:
+
+            self.file.append(fills)
+    
+    
     def menu(self,navBar):#I need a list? User input.. so dictorary inside of an array for rn
 
         self.navBar = navbar
@@ -17,3 +32,17 @@ class FileList:
         navBar = {"(1)":"Folders","(2)":"Settings"}#Gotta remember to add a .addpend method to the Kern not for this Navbar but probably the next
         pp.qprint(navBar)
 
+        qprint("<Input>")
+
+        Barput = input()
+
+        if Barput.islower(navBar.values):
+            print(fill.keys)
+
+        else:
+            Barput.islower(navbar.key)
+            pass # bc I dont have a file for that yet
+            #this is a decent starting point
+            
+
+        
