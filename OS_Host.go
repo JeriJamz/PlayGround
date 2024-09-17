6d6434432033bb820e91f203971f0459641c0c7c
Jerimaine Hunt <100004627+JeriJamz@users.noreply.github.com>
2024-09-17 12:29:47 -0500

    Create OS_Host.go

---
+++ OS_Host.go
@@ -0,0 +1,26 @@
+// I need to make a server server... guess data base? idk
+/*
+I
+Got
+Hungry
+*///Plus I gotta read on this port connection is stupid
+package main
+
+import("fmt"
+      "net"
+      )
+
+func main(){
+
+    ear, err := net.Listen("tcp":"9001")
+    if err != nil{
+
+        log.Fatal(err)
+    
+  }
+    
+    conn,err := l.Accept()
+    
+}
+
+
