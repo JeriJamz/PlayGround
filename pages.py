dbbb624fef2762611b53eea7b83ed71d6a4b29ea
Jerimaine Hunt <100004627+JeriJamz@users.noreply.github.com>
2024-09-19 16:25:50 -0500

    Update pages.py

--- pages.py
+++ pages.py
@@ -5,20 +5,29 @@ import curses,os
 
 class papyrus:
     
-    def __init__(self):
+    def __init__(self,scribe):
         
-        self.tab = 
+#        self.tab =  dow
+        
     
     def scribe(self):
 
+        #andd stop on notepad gone be right here. I need a setting file and way to save stuff
         wind = curses.initscr()
+        wind.border(5,5,25,15,0,0,0,0) #The boarders of the notepad app. Still import most of the funcs also the menu?!? 
         wind.cbreak(False)
         wind.echo(False)
         wind.keypad(True)
         wind.getmouse()
 
     def save(self):
-
-        if wind:
-            wind.putwin(os.path('edition','x+',encoding = 'utf-8'))#its not right but you get the idea, ima add the function to notepad soon
+        for wind ++1:#this this function made my head hurt
+            lil_win = curses.newwin(50,150)
+            lil_win.addstr(25,55,"Save <Enter> Yes Or No\n\n",
+            "<Enter>"))#think I got it down I'm reading docs
+            response = input()
+            if response.islower() == "Yes":
+                wind.putwin(os.path('edition','x+',encoding = 'utf-8'))#I think this is closer but I need to act like its local and not ontop of an O.S
+            else:
+               curses.endwin(lil_win())#Save Func          
 #FoodTime
